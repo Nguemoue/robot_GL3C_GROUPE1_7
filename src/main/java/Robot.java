ee1fa6a4e544923cd610c3e8a18b49def56c7f09
Scharonne <scharonnembao1@gmail.com>
2022-02-01 16:22:07 +0100

    ajout de la methode fire dans la classe robot

--- src/main/java/Robot.java
+++ src/main/java/Robot.java
@@ -1,37 +1,5 @@
 public class Robot {
-    private int vie;
-    private String nom;
-
-    /**
-     * @param nom
-     *  initialise un robot avec le nom $nom
-     */
-     Robot(String nom){
-        if(nom.equals("")){
-            System.out.println("vous devez entrez un nom valide");
-            System.exit(1);
-        }
-        this.nom = nom;
-        this.vie = 10;
-    }
-
-
-    public String getNom() {
-        return nom;
-    }
-
-    public int getVie() {
-        return vie;
-    }
-
-    public void setVie(int vie) {
-        this.vie = vie;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
+    
     public  boolean fire(Robot r){
         if(!r.isDead() && r.getVie() >=2){
             r.setVie(r.getVie() - 2);
@@ -41,12 +9,5 @@ public class Robot {
 
         return  false;
     }
-    public boolean isDead(){
-        return this.getVie() == 0;
-    }
-
-    @Override
-    public String toString(){
-        return  "Robot "+this.getNom();
-    }
+    
 }
