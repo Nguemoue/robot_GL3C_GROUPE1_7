0cbee3b96d93056af724920160633e106490d04a
Danielle <danielletcheuffa8@gmail.com>
2022-02-01 18:17:36 +0100

    ajout de la fonction isDead

--- src/main/java/Robot.java
+++ src/main/java/Robot.java
@@ -6,22 +6,10 @@ public class Robot {
      * @param nom
      *  initialise un robot avec le nom $nom
      */
-     Robot(String nom){
-        if(nom.equals("")){
-            System.out.println("vous devez entrez un nom valide");
-            System.exit(1);
-        }
-        this.nom = nom;
-        this.vie = 10;
-    }
 
 
     public boolean isDead(){
         return this.getVie() == 0;
     }
 
-    @Override
-    public String toString(){
-        return  "Robot "+this.getNom();
-    }
 }
