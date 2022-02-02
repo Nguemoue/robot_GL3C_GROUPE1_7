1dcdd2fda33ccf9d23c5677a1efda38513842d18
Nguemoue <81599046+Nguemoue@users.noreply.github.com>
2022-02-02 13:57:54 +0100

    resolution des conflits sur la classe robot

--- src/main/java/Robot.java
+++ src/main/java/Robot.java
@@ -1,9 +1,7 @@
 public class Robot {
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
+   private int vie;
+    private String nom;
+
     
     public  boolean fire(Robot r){
         if(!r.isDead() && r.getVie() >=2){
@@ -14,15 +12,8 @@ public class Robot {
 
         return  false;
     }
-    
-=======
->>>>>>> e5ce7db86144bac841e18667534799101926e2ed
->>>>>>> 2c02aca895390b7511cb01ba1ca64cc2e81b7d08
-    private int vie;
-    private String nom;
-
+ 
     /**
-<<<<<<< HEAD
      * @param nom initialise un robot avec le nom $nom
      */
     Robot(String nom) {
@@ -33,24 +24,18 @@ public class Robot {
         this.nom = nom;
         this.vie = 10;
     }
-}
-=======
+
      * @param nom
      *  initialise un robot avec le nom $nom
      */
-<<<<<<< HEAD
-
 
     public boolean isDead(){
         return this.getVie() == 0;
     }
 
-=======
 
     public String toString(){
         return  "Robot "+this.getNom();
     }
->>>>>>> e5ba4ebc9f73e6ecb8f7fa8916bffe4f423d869e
->>>>>>> e5ce7db86144bac841e18667534799101926e2ed
+
 }
->>>>>>> 2c02aca895390b7511cb01ba1ca64cc2e81b7d08
