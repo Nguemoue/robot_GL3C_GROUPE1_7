eae045c7e4b7976dcbb914bec5f28464dc308808
Nkwamegni <nkwamegnikevine@gmail.com>
2022-02-02 15:38:35 +0100

    refactorisation des test

--- src/test/java/RobotTest.java
+++ src/test/java/RobotTest.java
@@ -2,21 +2,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class RobotTest {
     Robot D2R2  = new Robot("D2R2"), Data = new Robot("Data");
-        @org.junit.jupiter.api.Test
-        void fire() {
-            assertEquals(Data.getVie(),D2R2.getVie());
-        }
 
-        @org.junit.jupiter.api.Test
-        void isDead() {
-
-        }
+        //teste que les ,methodes toString renvoi la chaine de caractere attendu
         @org.junit.jupiter.api.Test
          void TestName(){
             assertEquals(D2R2.toString(),"Robot D2R2");
             assertEquals(Data.toString(),"Robot Data");
          }
 
+         //teste le combat d'un robot et d'un deuxiemme robot
         @org.junit.jupiter.api.Test
         void TestFight(){
             D2R2.fire(Data);
@@ -28,10 +22,10 @@ class RobotTest {
         @org.junit.jupiter.api.Test
         void TestDeadFire(){
             Data.setVie(0);
-            assertTrue(D2R2.fire(Data));
+            assertFalse(D2R2.fire(Data));
 
         }
-
+        //teste le tir d'un roobot sur un autre
         @org.junit.jupiter.api.Test
         void TestFire(){
             //je reinitialise les points de vie des differents robots
